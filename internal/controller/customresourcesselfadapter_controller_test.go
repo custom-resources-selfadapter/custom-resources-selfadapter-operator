14d725b87761f9aafc0f3a4f47012de8c9252815
Humberto Fraga <xisberto@gmail.com>
2025-07-02 16:55:50 -0300

    Kubebuilder create API

---
+++ internal/controller/customresourcesselfadapter_controller_test.go
@@ -0,0 +1,84 @@
+/*
+Copyright 2025 The Custom Resources Self-Adapter Developers.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	customresourcesselfdapternetv1 "github.com/custom-resources-selfadapter/custom-resoursces-selfadapter-operator/api/v1"
+)
+
+var _ = Describe("CustomResourcesSelfadapter Controller", func() {
+	Context("When reconciling a resource", func() {
+		const resourceName = "test-resource"
+
+		ctx := context.Background()
+
+		typeNamespacedName := types.NamespacedName{
+			Name:      resourceName,
+			Namespace: "default", // TODO(user):Modify as needed
+		}
+		customresourcesselfadapter := &customresourcesselfdapternetv1.CustomResourcesSelfadapter{}
+
+		BeforeEach(func() {
+			By("creating the custom resource for the Kind CustomResourcesSelfadapter")
+			err := k8sClient.Get(ctx, typeNamespacedName, customresourcesselfadapter)
+			if err != nil && errors.IsNotFound(err) {
+				resource := &customresourcesselfdapternetv1.CustomResourcesSelfadapter{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      resourceName,
+						Namespace: "default",
+					},
+					// TODO(user): Specify other spec details if needed.
+				}
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+			}
+		})
+
+		AfterEach(func() {
+			// TODO(user): Cleanup logic after each test, like removing the resource instance.
+			resource := &customresourcesselfdapternetv1.CustomResourcesSelfadapter{}
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Cleanup the specific resource instance CustomResourcesSelfadapter")
+			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+		})
+		It("should successfully reconcile the resource", func() {
+			By("Reconciling the created resource")
+			controllerReconciler := &CustomResourcesSelfadapterReconciler{
+				Client: k8sClient,
+				Scheme: k8sClient.Scheme(),
+			}
+
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
+			// Example: If you expect a certain status condition after reconciliation, verify it here.
+		})
+	})
+})
